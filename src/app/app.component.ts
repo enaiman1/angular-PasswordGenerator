73268037f782bda1772ca47ccc554f96e56be73d
Ericcentrik <enaiman1@gmail.com>
2020-03-10 14:10:04 -0400

    added a method to take in the value of the enter length input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  length = 0
   includeLetters = false;
   includeNumbers = false;
   includeSymbols = false;
   password = "";
 
+  onChangeLength(value: string){
+    const parsedValue = parseInt(value);
+
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
+
   onChangeUseLetters(){
+    // this will change the letters property is selected, the opposite of its current state
     this.includeLetters = !this.includeLetters
   }
 
   onChangeUseNumbers(){
+    // this will change the numbers property is selected, opposite of its current state
     this.includeNumbers = !this.includeNumbers
   }
 
   onChangeUseSymbols() {
+    // this will change the symbols property is selected, opposite of its current state
     this.includeSymbols = !this.includeSymbols
   }
 
  onButtonClick(){
-   console.log(`use letters: ${this.includeLetters}`);
-   console.log(`use numbers: ${this.includeNumbers}`);
-   console.log(`use symbols: ${this.includeSymbols}`);
+   console.log(`
+   About to genrate a password with the following:
+   Includes letters: ${this.includeLetters}
+   Includes numbers: ${this.includeNumbers}
+   Includes symbols: ${this.includeSymbols}
+   `);
+
    
    this.password = "My Password!!!"
  }
